7aeab435623bd6e21466f93a867598a75780ecd8
katelarionova2058 <92648690+katelarionova2058@users.noreply.github.com>
2021-12-07 15:34:39 +0300

    Update station.rb

--- station.rb
+++ station.rb
@@ -1,1 +1,51 @@
+require_relative 'instance_counter.rb'
+require_relative 'validate.rb'
 
+class Station
+
+  include InstanceCounter
+  include Validate
+
+  @@all = []
+
+  attr_reader :name, 
+              :trains_on_station
+
+  def initialize(name)
+    @@all.push(self)
+    @name = name  
+    @trains_on_station = []
+    validate!
+    #register_instance
+  end
+
+  def add_trains (train)
+    @trains_on_station << train
+  end
+
+  def del_trains (train)
+    trains_on_station.delete(train)
+  end
+
+  def show_trains
+    @trains_on_station.each { |train| puts train.name }
+  end
+
+  def return_type(type)
+    count_of_type = @trains_on_station.select { |train| train.type == type }
+    count_of_type.count
+  end
+
+  def self.all
+    @@all
+  end
+
+  protected
+
+  def validate!
+    raise "Название не может быть пустым" if name.empty?
+    raise "Номер не может иметь длину больше 15 символов" if name.length > 15
+    true
+  end
+
+end
